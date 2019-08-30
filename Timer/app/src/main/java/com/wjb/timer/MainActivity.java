8cd9a178cd64771ea5128a8e2e1fb0a6d56e194a
Mr-Wes <dmsmc@outlook.com>
2019-08-31 02:15:16 +0800

    20190830

---
+++ Timer/app/src/main/java/com/wjb/timer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wjb.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO: 2019/8/31 加载xml文件，解析项目条目，根据这些条目创建相应个数的activity 
+    }
+}
